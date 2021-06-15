164d925c7dad6cb1078ea1143eaeeb382bf0a67c
Valkimani <66477953+Valkimani@users.noreply.github.com>
2021-06-15 08:20:20 -0400

    Created a script.js file and added a weather object.

---
+++ script.js
@@ -0,0 +1,45 @@
+
+// let cities = ["London", "SA", "Berlin"];
+// alert ("There are 3 cities.")
+// console.log(cities)
+
+// function showCity(city){
+//     console.log(city)
+// }
+
+// forEach is a function that will call every city in the array
+
+// cities.forEach(showCity);
+
+// function showCity(cityElement){
+    // Cities are element in the html file
+    // console.log(cityElement.innerHTML);
+// }
+// select all the Li
+// let cities = document.querySelectorAll("li");
+
+// go through each city and call the function showCity
+// cities.forEach(showCity);
+
+let weather={
+    "paris":{
+        temp: 19.7,
+        humidity:80
+    },
+    "tokyo":{
+        temp: 17.3,
+        humidity:50
+    },
+    "lisbon":{
+        temp: 30.2,
+        humidity:20
+    },
+    "paris":{
+        temp: 20.9,
+        humidity:100
+    },
+    "paris":{
+        temp: -5,
+        humidity:20
+    },
+}
